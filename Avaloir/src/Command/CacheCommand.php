5e3e0d3dc056b4cca32029ecd72217454555cbd1
jfsenechal <jfsenechal@gmail.com>
2023-02-16 13:37:26 +0100

    add command

---
+++ Avaloir/src/Command/CacheCommand.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace AcMarche\Avaloir\Command;
+
+use AcMarche\Avaloir\Repository\AvaloirRepository;
+use AcMarche\Stock\Service\SerializeApi;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+#[AsCommand(
+    name: 'avaloir:cache',
+    description: 'Generate cache'
+)]
+class CacheCommand extends Command
+{
+    public function __construct(
+        private AvaloirRepository $avaloirRepository,
+        private SerializeApi $serializeApi
+    ) {
+        parent::__construct();
+    }
+
+    protected function configure(): void
+    {
+        $this
+            ->addArgument('latitude')
+            ->addArgument('longitude');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $this->io = new SymfonyStyle($input, $output);
+        $this->serializeApi->serializeAvaloirs($this->avaloirRepository->getAll());
+
+        return Command::SUCCESS;
+    }
+}
