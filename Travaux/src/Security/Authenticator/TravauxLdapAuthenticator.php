8099b36039345f0dc81e40c22c44890539b1da1e
jfsenechal <jfsenechal@gmail.com>
2023-03-30 16:57:25 +0200

    add remember_me

--- Travaux/src/Security/Authenticator/TravauxLdapAuthenticator.php
+++ Travaux/src/Security/Authenticator/TravauxLdapAuthenticator.php
@@ -16,6 +16,7 @@ use Symfony\Component\Security\Core\Security;
 use Symfony\Component\Security\Http\Authenticator\AbstractLoginFormAuthenticator;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\CsrfTokenBadge;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\PasswordUpgradeBadge;
+use Symfony\Component\Security\Http\Authenticator\Passport\Badge\RememberMeBadge;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
 use Symfony\Component\Security\Http\Authenticator\Passport\Credentials\PasswordCredentials;
 use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
@@ -53,6 +54,7 @@ class TravauxLdapAuthenticator extends AbstractLoginFormAuthenticator
 
         $badges =
             [
+                new RememberMeBadge(),
                 new CsrfTokenBadge('authenticate', $token),
                 new PasswordUpgradeBadge($password, $this->userRepository), //SelfValidatingPassport?
             ];
