b3e49c6463db694fbd5b22f406a4da43dee91e11
jfsenechal <jfsenechal@gmail.com>
2024-01-24 09:37:19 +0100

    up sf 7.0 asset map

--- Travaux/assets/app.js
+++ Travaux/assets/app.js
@@ -1,19 +1,11 @@
+import './bootstrap.js';
 /*
  * Welcome to your app's main JavaScript file!
  *
- * We recommend including the built version of this JavaScript file
- * (and its CSS file) in your base layout (base.html.twig).
+ * This file will be included onto the page via the importmap() Twig function,
+ * which should already be in your base.html.twig.
  */
-
-// any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.css';
 
-// start the Stimulus application
-import './bootstrap';
-
-import 'bootstrap';
-
-import '@fortawesome/fontawesome-free/css/all.css';
-import '@fortawesome/fontawesome-free/js/all.js';
-import 'leaflet';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'leaflet/dist/leaflet.min.css'
--- Travaux/assets/bootstrap.js
+++ Travaux/assets/bootstrap.js
@@ -1,16 +1,5 @@
-import { startStimulusApp } from '@symfony/stimulus-bridge';
-import { Autocomplete } from 'stimulus-autocomplete';
-import Lightbox from "stimulus-lightbox"
+import { startStimulusApp } from '@symfony/stimulus-bundle';
 
-// Registers Stimulus controllers from controllers.json and in the controllers/ directory
-export const app = startStimulusApp(require.context(
-    '@symfony/stimulus-bridge/lazy-controller-loader!./controllers',
-    true,
-    /\.[jt]sx?$/
-));
-
+const app = startStimulusApp();
 // register any custom, 3rd party controllers here
 // app.register('some_controller_name', SomeImportedController);
-
-app.register('autocomplete', Autocomplete);
-app.register('lightbox', Lightbox);
