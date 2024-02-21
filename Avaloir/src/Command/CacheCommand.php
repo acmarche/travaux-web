e7239cc170937b493aa9ea0d286d1e05f1bd282c
jfsenechal <jfsenechal@gmail.com>
2024-02-21 10:45:23 +0100

    up

--- Avaloir/src/Command/CacheCommand.php
+++ Avaloir/src/Command/CacheCommand.php
@@ -4,11 +4,13 @@ namespace AcMarche\Avaloir\Command;
 
 use AcMarche\Avaloir\Repository\AvaloirRepository;
 use AcMarche\Stock\Service\SerializeApi;
+use Psr\Cache\InvalidArgumentException;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Contracts\Cache\CacheInterface;
 
 #[AsCommand(
     name: 'avaloir:cache',
@@ -17,23 +19,35 @@ use Symfony\Component\Console\Style\SymfonyStyle;
 class CacheCommand extends Command
 {
     public function __construct(
-        private AvaloirRepository $avaloirRepository,
-        private SerializeApi $serializeApi
+        private readonly AvaloirRepository $avaloirRepository,
+        private readonly SerializeApi $serializeApi,
+        private readonly CacheInterface $cache,
     ) {
         parent::__construct();
     }
 
     protected function configure(): void
     {
-        $this
-            ->addArgument('latitude')
-            ->addArgument('longitude');
+
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $io = new SymfonyStyle($input, $output);
-        $this->serializeApi->serializeAvaloirs($this->avaloirRepository->getAll());
+        $date = "";
+        $last = $this->avaloirRepository->getLastUpdatedAvaloir();
+
+        if ($last) {
+            $date = $last->getUpdatedAt()->format('Y-m-d H');
+        }
+
+        try {
+            $this->cache->get('allAvaloirs-'.$date, function () {
+                $this->serializeApi->serializeAvaloirs($this->avaloirRepository->getAll());
+            });
+        } catch (InvalidArgumentException $e) {
+            $io->error($e->getMessage());
+        }
 
         return Command::SUCCESS;
     }
