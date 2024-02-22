fb7f4fb993c816ba9748cefcf5fb8c785d89e12d
jfsenechal <jfsenechal@gmail.com>
2024-02-22 09:28:11 +0100

    new config file

--- Travaux/config/packages/security.php
+++ Travaux/config/packages/security.php
@@ -3,49 +3,19 @@
 use AcMarche\Travaux\Entity\Security\User;
 use AcMarche\Travaux\Security\Authenticator\TravauxAuthenticator;
 use AcMarche\Travaux\Security\Authenticator\TravauxLdapAuthenticator;
-use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symfony\Component\Ldap\Ldap;
 use Symfony\Component\Ldap\LdapInterface;
 use Symfony\Config\SecurityConfig;
 
-return static function (ContainerConfigurator $containerConfigurator): void {
-    $containerConfigurator->extension('security', [
-        'password_hashers' => [
-            User::class => [
-                'algorithm' => 'auto',
-            ],
+return static function (SecurityConfig $security) {
+    $security->provider('travaux_user_provider', [
+        'entity' => [
+            'class' => User::class,
+            'property' => 'username',
         ],
     ]);
 
-    $containerConfigurator->extension(
-        'security',
-        [
-            'providers' => [
-                'travaux_user_provider' => [
-                    'entity' => [
-                        'class' => User::class,
-                        'property' => 'username',
-                    ],
-                ],
-            ],
-        ]
-    );
-
     $authenticators = [TravauxAuthenticator::class];
-
-    $main = [
-        'provider' => 'travaux_user_provider',
-        'logout' => [
-            'path' => 'app_logout',
-        ],
-        'form_login' => [],
-        'entry_point' => TravauxAuthenticator::class,
-        'switch_user' => true,
-        'login_throttling' => [
-            'max_attempts' => 6, //per minute...
-        ],
-    ];
-
     if (interface_exists(LdapInterface::class)) {
         $authenticators[] = TravauxLdapAuthenticator::class;
         $main['form_login_ldap'] = [
@@ -54,36 +24,26 @@ return static function (ContainerConfigurator $containerConfigurator): void {
         ];
     }
 
-    $main['custom_authenticator'] = $authenticators;
+    // @see Symfony\Config\Security\FirewallConfig
+    $main = [
+        'provider' => 'travaux_user_provider',
+        'logout' => [
+            'path' => 'app_logout',
+        ],
+        'form_login' => [],
+        'entry_point' => TravauxAuthenticator::class,
+        'custom_authenticators' => $authenticators,
+        'login_throttling' => [
+            'max_attempts' => 6, // per minute...
+        ],
+        'remember_me' => [
+            'secret' => '%kernel.secret%',
+            'lifetime' => 604800,
+            'path' => '/',
+            'always_remember_me' => true,
+        ],
+    ];
 
-    $containerConfigurator->extension(
-        'security',
-        [
-            'firewalls' => [
-                'main' => $main,
-            ],
-        ]
-    );
-
-    $containerConfigurator->extension(
-        'security',
-        [
-            'role_hierarchy' => [
-                'ROLE_FINANCE_FRAIS_ADMIN' => ['ROLE_FINANCE_FRAIS'],
-            ],
-        ]
-    );
-};
-
-return static function (SecurityConfig $security) {
-    $security
-        ->provider('travaux_user_provider')
-        ->entity()
-        ->class(User::class)
-        ->property('username');
-
-    $security->passwordHasher(User::class, [
-            'algorithm' => 'auto',
-        ]
-    );
+    $security->firewall('main', $main)
+        ->switchUser();
 };
