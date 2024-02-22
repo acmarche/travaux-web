ec23ae011e727ade4c9d391e6593b57a074d7478
jfsenechal <jfsenechal@gmail.com>
2024-02-22 15:26:10 +0100

    clean

--- Travaux/src/Form/PlanningType.php
+++ Travaux/src/Form/PlanningType.php
@@ -11,7 +11,6 @@ use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -51,8 +50,9 @@ class PlanningType extends AbstractType
                 'entry_type' => DateType::class,
                 'label' => 'Dates',
                 'required' => true,
-                'entry_options' => ['label' => false, 'widget' => 'single_text'],
+                'entry_options' => ['label' => false],
                 'allow_add' => true,
+                'allow_delete' => true,
                 'by_reference' => false,
                 'attr' => [
                     'autocomplete' => 'off',
