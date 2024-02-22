74e726306d42842cc3a18867b59fa63bd6bb926b
jfsenechal <jfsenechal@gmail.com>
2024-02-22 09:57:28 +0100

    print and add missing

--- Travaux/src/Controller/PlanningController.php
+++ Travaux/src/Controller/PlanningController.php
@@ -60,12 +60,13 @@ class PlanningController extends AbstractController
 
         $interventions = $this->interventionPlanningRepository->findByMonthAndCategory($yearmonth, $categoryPlanning);
         $days = $this->dateProvider->daysOfMonth($dateSelected);
-        $data = [];
+        $data = $absents = [];
         foreach ($days as $day) {
             $data[$day->day] = $this->interventionPlanningRepository->findPlanningByDayAndCategory(
                 $day,
                 $categoryPlanning
             );
+            $absents[$day->day] = $this->absenceUtils->findAbsentByDateAndCategory($day, $categoryPlanning);
         }
 
         $next = $dateSelected->addMonth();
@@ -87,6 +88,7 @@ class PlanningController extends AbstractController
             'previous' => $previous,
             'weeks' => $weeks,
             'data' => $data,
+            'absents' => $absents,
             'categorySelected' => $categoryPlanning,
             'categories' => $this->categoryPlanningRepository->findAllOrdered(),
         ]);
